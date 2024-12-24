eb8a840cd818ee58295dc5bcecddcd6326791f73
Alperen Tan <lprntan@gmail.com>
2024-12-24 12:10:24 +0300

    EntityLayer created

---
+++ InveonBootcamp.MiniCourseApp/EntityLayer/Concrete/UserApp.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class UserApp : IdentityUser
+    {
+        public string? FullName { get; set; }
+        public ICollection<Course>? MyCourses { get; set; }
+    }
+}
