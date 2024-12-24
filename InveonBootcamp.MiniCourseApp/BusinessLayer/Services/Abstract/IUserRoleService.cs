7d947c194223e991870f56bd98259c06637b3427
Alperen Tan <lprntan@gmail.com>
2024-12-24 18:40:39 +0300

    UserRole service added

---
+++ InveonBootcamp.MiniCourseApp/BusinessLayer/Services/Abstract/IUserRoleService.cs
@@ -0,0 +1,15 @@
+﻿using CoreLayer.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Services.Abstract
+{
+    public interface IUserRoleService
+    {
+        Task<ResponseDto<string>> CreateRoleAsync(string roleName);
+        Task<ResponseDto<string>> AssignRoleToUserAsync(string email, string roleName);
+    }
+}
--- InveonBootcamp.MiniCourseApp/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
+++ InveonBootcamp.MiniCourseApp/BusinessLayer/obj/Debug/net8.0/BusinessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4312b2b4e7b87d78f7d3a4574f39669986480c7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f544abde85e58165ad73c179ebd70c0115598760")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
