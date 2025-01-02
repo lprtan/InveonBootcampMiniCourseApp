a92fe532eace4dc8c8254bd89633a6a94253ba15
Alperen Tan <lprntan@gmail.com>
2025-01-02 15:32:12 +0300

    added new Presentation Layer

--- InveonBootcamp.MiniCourseApp/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
+++ InveonBootcamp.MiniCourseApp/DataAccessLayer/obj/Debug/net8.0/DataAccessLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ccf54f6c0013e28d581479863c6350b2aab7c35f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ef5e8e7a8341a3667419465566587a7d7be447")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccessLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
