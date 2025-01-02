93284f9e0950575cebbe31217fb5fe1448acc576
Alperen Tan <lprntan@gmail.com>
2025-01-02 15:53:22 +0300

    ReactApp created

--- InveonBootcamp.MiniCourseApp/PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
+++ InveonBootcamp.MiniCourseApp/PresentationLayer/obj/Debug/net8.0/PresentationLayer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3ef5e8e7a8341a3667419465566587a7d7be447")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a92fe532eace4dc8c8254bd89633a6a94253ba15")]
 [assembly: System.Reflection.AssemblyProductAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PresentationLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
