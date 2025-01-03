9b2bcfe0da90b180d83602ef00f85d51092b75ba
Alperen Tan <lprntan@gmail.com>
2025-01-03 21:37:35 +0300

    Log out added

--- InveonBootcamp.MiniCourseApp/PresentationLayer/client/course/src/components/ProfileMenu.js
+++ InveonBootcamp.MiniCourseApp/PresentationLayer/client/course/src/components/ProfileMenu.js
@@ -1,23 +1,43 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
 import { FaUser, FaSignOutAlt } from "react-icons/fa";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 import "../styles/profilMenu.css";
 
-function ProfileMenu({ onProfileClick, onLogoutClick }) {
+function ProfileMenu({ onProfileClick }) {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const refreshToken = localStorage.getItem("refreshToken");
+
+    try {
+
+      await axios.post("https://localhost:7037/api/Auth/RevokeRefreshToken", {
+        token: refreshToken,
+      });
+
+
+      localStorage.removeItem("accessToken");
+      localStorage.removeItem("refreshToken");
+
+
+      navigate("/");
+    } catch (error) {
+      console.error("Çıkış yaparken bir hata oluştu:", error);
+    }
+  };
+
   return (
     <Dropdown align="end">
-      <Dropdown.Toggle
-        variant="light"
-        id="dropdown-basic"
-        className="profile-icon-toggle"
-      >
+      <Dropdown.Toggle variant="light" id="dropdown-basic" className="profile-icon-toggle">
         <FaUser className="profile-icon" />
       </Dropdown.Toggle>
       <Dropdown.Menu>
         <Dropdown.Item onClick={onProfileClick}>
           <FaUser style={{ marginRight: "8px" }} /> Profil
         </Dropdown.Item>
-        <Dropdown.Item onClick={onLogoutClick}>
+        <Dropdown.Item onClick={handleLogout}>
           <FaSignOutAlt style={{ marginRight: "8px" }} /> Çıkış Yap
         </Dropdown.Item>
       </Dropdown.Menu>
@@ -26,4 +46,3 @@ function ProfileMenu({ onProfileClick, onLogoutClick }) {
 }
 
 export default ProfileMenu;
-
